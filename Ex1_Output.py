d4d8d5ab24cb12c95cd79d9fa08b586f06ddae4a
toppy1st <143517437+toppy1st@users.noreply.github.com>
2024-09-03 17:39:04 -0400

    Add files via upload

---
+++ Ex1_Output.py
@@ -0,0 +1,6 @@
+# Intro Examples
+# using print() function to display
+
+print("Hello World")
+print("Hello python World")
+print("Hello ET574")
---
+++ Ex2_Input.py
@@ -0,0 +1,6 @@
+# Intro Examples
+# using input() & print() function to display
+
+course  = input("Enter your course: ")
+name = input ("What is your name? \n")
+print("Welcome to " + course +" "+ name + '\n!')
