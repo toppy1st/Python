52e8c9079e27273eff2bb372fe7cea4a27a4b16f
toppy1st <143517437+toppy1st@users.noreply.github.com>
2024-09-05 17:11:09 -0400

    Add files via upload

---
+++ #Programming Exercise - Variables-2.py
@@ -0,0 +1,9 @@
+#Programming Exercise - Variables
+
+#2
+
+apple = float(1.75)
+banana = float(2.25)
+cherry = float(3.50)
+
+print ("Item\tPrice\nApple\t$", apple, "\nBanana\t$",banana,"\nCherry\t$",cherry,"\n-------\t------\n","Total\t$",apple+banana+cherry)
